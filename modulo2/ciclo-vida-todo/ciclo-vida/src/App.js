d269e059a5b631b5d8cc0dd39c3dadaf956f0b2c
Leonardo-almd <104574298+Leonardo-almd@users.noreply.github.com>
2022-06-14 15:47:02 -0300

    ciclo-de-vida

---
+++ modulo2/ciclo-vida-todo/ciclo-vida/src/App.js
@@ -0,0 +1,116 @@
+import React from 'react'
+import styled from 'styled-components'
+import './styles.css'
+
+const TarefaList = styled.ul`
+  padding: 0;
+  width: 200px;
+`
+
+const Tarefa = styled.li`
+  text-align: left;
+  text-decoration: ${({ completa }) => (completa ? 'line-through' : 'none')};
+`
+
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+`
+
+class App extends React.Component {
+  state = {
+    tarefas: [],
+    inputValue: '',
+    filtro: ''
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('tarefas', JSON.stringify(this.state.tarefas))
+  };
+
+  componentDidMount() {  
+   const pegarDados = localStorage.getItem('tarefas')
+   this.setState({tarefas: JSON.parse(pegarDados)})         
+  }  
+
+  onChangeInput = (event) => {
+    this.setState({ inputValue: event.target.value })
+  } 
+
+  criaTarefa = () => {
+    const novaTarefa = {
+      id: Date.now(),
+      texto: this.state.inputValue,
+      completa: false
+    }
+
+    const copia = [novaTarefa, ...this.state.tarefas]
+
+    this.setState({ tarefas: copia })
+  }
+
+  selectTarefa = (id) => {
+     const idTarefas = this.state.tarefas.map((dever) => {
+      if(id == dever.id) {
+        const novoObjeto = {
+          ...dever,
+          completa: !dever.completa      
+        }
+        return novoObjeto
+      }
+      return idTarefas
+     })
+  }
+
+  onChangeFilter = (event) => {
+    this.setState({ filtro: event.target.value })
+  }
+
+  render() {
+    const listaFiltrada = this.state.tarefas.filter(tarefa => {
+      switch (this.state.filtro) {
+        case 'pendentes':
+          return !tarefa.completa
+        case 'completas':
+          return tarefa.completa
+        default:
+          return true
+      }
+    })
+
+    return (
+      <div className="App">
+        <h1>Lista de tarefas</h1>
+        <InputsContainer>
+          <input value={this.state.inputValue} onChange={this.onChangeInput} />
+          <button onClick={this.criaTarefa}>Adicionar</button>          
+        </InputsContainer>
+        <br />
+
+        <InputsContainer>
+          <label>Filtro</label>
+          <select value={this.state.filter} onChange={this.onChangeFilter}>
+            <option value="">Nenhum</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="completas">Completas</option>
+          </select>
+        </InputsContainer>
+        <TarefaList>
+          {listaFiltrada.map(tarefa => {
+            return (
+              <Tarefa
+                completa={tarefa.completa}
+                onClick={() => this.selectTarefa(tarefa.id)}
+              >
+                {tarefa.texto}
+              </Tarefa>
+            )
+          })}
+        </TarefaList>
+      </div>
+    )
+  }
+}
+
+export default App
