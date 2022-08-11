886d699b7210acc158c3017db8d4f11f639c2946
Leonardo-almd <104574298+Leonardo-almd@users.noreply.github.com>
2022-08-11 15:12:53 -0300

    aprofundamento-typescript

---
+++ modulo4/aprofundamento-typescript/src/exercicio-4.ts
@@ -0,0 +1,23 @@
+type Pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: Pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: Pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: Pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
